9248800dcfa2b9d6f0e498a81db5b3c9ce6bda0f
pecheritsa <pecheritsa.roman@yandex.ru>
2024-05-17 00:11:16 +0300

    Hotfix str_func, Update README

--- str_func.py
+++ str_func.py
@@ -1,9 +1,9 @@
 def upper_text(text):
     """Capitalized text"""
-    return text.upper()
+    return text.upper() + f'<-- This text is upper'
 
 
 def title_text(text):
     """Titile text"""
-    return text.title()
+    return text.title() + f'<-- This text is title'
 
